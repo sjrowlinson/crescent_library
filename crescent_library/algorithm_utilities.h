71cfdab281625ec106e55e0350986c12d687e793
Samuel Rowlinson <sjrowlinson@outlook.com>
2016-08-08 15:46:52 +0100

    Added find_all, find_all_if and find_all_if_not methods.

--- crescent_library/algorithm_utilities.h
+++ crescent_library/algorithm_utilities.h
@@ -3,10 +3,10 @@
 #include <algorithm>
 #include <numeric>
 #include <iostream>
+#include <set>
 #include <vector>
 
 namespace crsc {
-
 	/**
 	 * \brief Tag-sorts a `std::vector` returning the sorted vector of indices.
 	 *
@@ -24,7 +24,57 @@ namespace crsc {
 		});
 		return tag_vec;
 	}
-
+	/**
+	 * \brief Searches for elements with value `value` in the range `[first, last)` and returns
+	 *        a `std::set<InputIt>` containing all occurrences of this element.
+	 *
+	 * \param first Beginning of the range to examine.
+	 * \param last End of range to examine.
+	 * \param value Value to compare the elements to.
+	 * \return `std::set<InputIt>` containing iterators to all occurrences of `value` in the range.
+	 */
+	template<class InputIt, class _Ty>
+	std::set<InputIt> find_all(InputIt first, InputIt last, const _Ty& value) {
+		std::set<InputIt> found_set;
+		for (InputIt it = first; it != last; ++it) {
+			if (*it == value) found_set.insert(it);
+		}
+		return found_set;
+	}
+	/**
+	 * \brief Searches for elements in the range `[first, last)` for which the predicate `p` returns
+	 *        `true` and returns a `std::set<InputIt>` containing all occurrences of these elements.
+	 *
+	 * \param first Beginning of range to examine.
+	 * \param last End of range to examine.
+	 * \param p Unary predicate which returns `true` for the required elements.
+	 * \return `std::set<InputIt>` containing iterators to all occurrencs for which `p` yields `true`.
+	 */
+	template<class InputIt, class UnaryPredicate>
+	std::set<InputIt> find_all_if(InputIt first, InputIt last, UnaryPredicate p) {
+		std::set<InputIt> found_set;
+		for (InputIt it = first; it != last; ++it) {
+			if (p(*it)) found_set.insert(it);
+		}
+		return found_set;
+	}
+	/**
+	 * \brief Searches for elements in the range `[first, last)` for which the predicate `q` returns
+	 *        `false` and returns a `std::set<InputIt>` containing all occcurrences of these elements.
+	 *
+	 * \param first Beginning of range to examine.
+	 * \param last End of range to examine.
+	 * \param q Unary predicate which return `false` for the required elements.
+	 * \return `std::set<InputIt>` containing iterators to all occurrencs for which `q` yields `false`.
+	 */
+	template<class InputIt, class UnaryPredicate>
+	std::set<InputIt> find_all_if_not(InputIt first, InputIt last, UnaryPredicate q) {
+		std::set<InputIt> found_set;
+		for (InputIt it = first; it != last; ++it) {
+			if (!q(*it)) found_set.insert(it);
+		}
+		return found_set;
+	}
 }
 
 #endif
