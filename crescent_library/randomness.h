6904bc8f7417ce49d55f0d95968fa928e45db53c
Samuel Rowlinson <sjrowlinson@outlook.com>
2016-08-29 22:03:02 +0100

    Improved existing random generator wrappers and added crsc::random_complex_generator.

--- crescent_library/randomness.h
+++ crescent_library/randomness.h
@@ -1,3 +1,4 @@
+#include <complex>
 #include <random>
 
 namespace crsc {
@@ -25,35 +26,103 @@ namespace crsc {
 		std::uniform_real_distribution<Ty> prob_dist;
 	};
 #endif // !RANDOM_PROBABILITY_GENERATOR_H
-	template<class Ty,
+#ifndef RANDOM_NUMBER_GENERATOR_H
+#define RANDOM_NUMBER_GENERATOR_H
+	/**
+	 * \class random_number_generator
+	 *
+	 * \brief Pseudo random number generator for random values over a specified `Distribution`
+	 *        using a given `Generator` engine.
+	 *
+	 * \tparam Ty The type of the values to generate, must satisfy `std::is_arithmetic<Ty>`. Defaults
+	 *         to the integral type `int`.
+	 * \tparam Generator The type of the generator engine to use for pseudo-random generation, must
+	 *         meet the requirement of `UniformRandomBitGenerator` (see C++ Concepts). Defaults to
+	 *         the engine type `std::mt19937`.
+	 * \tparam Distribution The type of distribution over which to calculate the random integers. The
+	 *         value type of the distribution must match the value type `Ty` of this class. Defaults
+	 *         to the distribution type `std::uniform_int_distribution<Ty>`.
+	 */
+	template<class Ty = int,
 		class Generator = std::mt19937,
-		class = std::enable_if_t<std::is_integral<Ty>::value>
-	> class uniform_int_random_generator {
+		class Distribution = std::uniform_int_distribution<Ty>,
+		class = std::enable_if_t<std::is_arithmetic<Ty>::value>
+	> class random_number_generator {
 	public:
-		uniform_int_random_generator(const Ty& min = 0, const Ty& max = std::numeric_limits<Ty>::max())
-			: eng{ std::random_device{}() }, dist(min, max) {}
+		/**
+		 * \brief Move constructs the generator with the values of the engine `_eng` and the
+		 *        distribution `_dist`. This is also the default constructor.
+		 *
+		 * \param _eng Generator engine to use.
+		 * \param _dist Distribution for random numbers.
+		 */
+		random_number_generator(Generator&& _eng = Generator{std::random_device{}()}, Distribution&& _dist = Distribution())
+			: eng(std::move(_eng)), dist(std::move(_dist)) {}
+		/**
+		 * \brief Constructs the generator with a copy of the values of the engine `_eng` and
+		 *        the distribution `_dist`.
+		 *
+		 * \param _eng Generator engine to use.
+		 * \param _dist Distribution for random numbers.
+		 */
+		random_number_generator(const Generator& _eng, const Distribution& _dist)
+			: eng(_eng), dist(_dist) {}
+		/**
+		 * \brief Copy constructor. Constructs the generator with a copy of the fields of `other`.
+		 *
+		 * \param other `random_number_generator` instance to use as data source.
+		 */
+		random_number_generator(const random_number_generator& other)
+			: eng(other.eng), dist(other.dist) {}
+		/**
+		 * \brief Move constructor. Constructs the generator with the fields of `other` using
+		 *        move-semantics such that `other` is left in a valid but unspecified state.
+		 *
+		 * \param other `random_number_generator` instance to use as data source.
+		 */
+		random_number_generator(random_number_generator&& other)
+			: eng(std::move(other.eng)), dist(std::move(other.dist)) {}
+		/**
+		 * \brief Generates the next random number in the distribution.
+		 *
+		 * \return The generated random number.
+		 */
 		Ty operator()() { return dist(eng); }
-		Ty min() const { return dist.min(); }
-		Ty max() const { return dist.max(); }
 	private:
 		Generator eng;
-		std::uniform_int_distribution<Ty> dist;
+		Distribution dist;
 	};
-
-	template<class Ty,
+#endif // !RANDOM_NUMBER_GENERATOR_H
+#ifndef RANDOM_COMPLEX_GENERATOR_H
+#define RANDOM_COMPLEX_GENERATOR_H
+	/**
+	 * \class random_complex_generator
+	 *
+	 * \brief Generator for random complex numbers in `std::complex` object form.
+	 *
+	 * \tparam Ty The type of the real and imaginary parts of the complex number.
+	 * \tparam Generator The type of the generator engine to use for pseudo-random generation.
+	 * \tparam Distribution The type of distribution over which to calculate the random numbers.
+	 */
+	template<class Ty = double,
 		class Generator = std::mt19937,
-		class = std::enable_if_t<std::is_floating_point<Ty>::value>
-	> class uniform_fp_random_generator {
+		class Distribution = std::uniform_real_distribution<Ty>
+	> class random_complex_generator {
 	public:
-		uniform_fp_random_generator(const Ty& min, const Ty& max)
-			: eng{ std::random_device{}() }, dist(min, max) {}
-		Ty operator()() { return dist(eng); }
-		Ty min() const { return dist.min(); }
-		Ty max() const { return dist.max(); }
+		random_complex_generator(Generator&& _eng = Generator{std::random_device{}()}, Distribution&& _dist = Distribution())
+			: eng(std::move(_eng)), dist(std::move(_dist)) {}
+		random_complex_generator(const Generator& _eng, const Distribution& _dist)
+			: eng(_eng), dist(_dist) {}
+		random_complex_generator(const random_complex_generator& other)
+			: eng(other.eng), dist(other.dist) {}
+		random_complex_generator(random_complex_generator&& other)
+			: eng(std::move(other.eng)), dist(std::move(other.dist)) {}
+		std::complex<Ty> operator()() { return std::complex<Ty>(dist(eng), dist(eng)); }
 	private:
 		Generator eng;
-		std::uniform_real_distribution<Ty> dist;
+		Distribution dist;
 	};
+#endif // !RANDOM_COMPLEX_GENERATOR_H
 }
 
 
